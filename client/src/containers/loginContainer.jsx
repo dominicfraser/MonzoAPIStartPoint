c4b24d85980729f97cde36e21be4a5ec081190b8
Dominic Fraser <mrdjfraser@gmail.com>
2017-12-11 23:50:33 +0000

    post request in node express server to parse URL for code and state, and exchange for access token, saved in an express session

--- client/src/containers/loginContainer.jsx
+++ client/src/containers/loginContainer.jsx
@@ -8,8 +8,12 @@ class LoginContainer extends React.Component {
 
   render() {
 
+    const client_id = "oauthclient_00009RQPsv5sYsKREbaUDp"
+    const redirect_uri = "http://localhost/authorised"
+    const state = "fajfljahiofosanlfo7878989fa9s6af76ga"
+
     return (
-      <div><a href={"https://auth.getmondo.co.uk/?client_id=oauthclient_00009RQPsv5sYsKREbaUDp&redirect_uri=http://localhost/authorised&response_type=code&state=fajfljahiofosanlfo7878989fa9s6af76ga"}>Login to Monzo</a>
+      <div><a href={"https://auth.getmondo.co.uk/?client_id=" + client_id + "&redirect_uri=" + redirect_uri + "&response_type=code&state=" + state}>Login to Monzo</a>
       </div>
       )
   }
--- server.js
+++ server.js
@@ -3,11 +3,57 @@ const app = express()
 const bodyParser = require('body-parser')
 const path = require('path')
 const dotEnv = require('dotenv').load()
+const request = require('request')
+const session = require('express-session')
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: true}))
 
 app.use(express.static('client/build'))
+
+app.use(session({
+    resave: true,
+    saveUninitialized: true,
+    secret: process.env.SESSION_SECRET
+}))
+
+app.get('/authorised', (req, res) => {
+  console.log("sending POST request to exchange authorization code for access token")
+  
+  request.post(
+      'https://api.monzo.com/oauth2/token',
+      { form: {
+        grant_type: "authorization_code",
+        client_id: process.env.CLIENT_ID,
+        client_secret: process.env.CLIENT_SECRET,
+        redirect_uri: process.env.REDIRECT_URI,
+        code: req.query.code
+        }
+      },
+      (error, response, body) => {
+        if (response.statusCode !== 200) {
+          console.log('Not 200')
+          if (error) {
+            console.log('Error: ', error)
+          }
+          return 
+        } else {
+          res.sendFile(path.join(__dirname + '/client/build/index.html'))
+
+          if(process.env.STATE === req.query.state){
+            //Monzo Docs: If this value differs from what you sent, you must abort the authentication process.
+
+            const bodyObj = JSON.parse(body)
+
+            req.session.access_token = bodyObj.access_token
+            req.session.expires_in = bodyObj.expires_in
+            req.session.refresh_token = bodyObj.refresh_token
+            req.session.user_id = bodyObj.user_id
+          }
+        } 
+      }
+  )
+})
 
 app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname + '/client/build/index.html'))
